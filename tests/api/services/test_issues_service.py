313d4fd028ad475498ad3916536b1647c2f0c3b0
David Moore <damoo@microsoft.com>
2025-02-05 13:49:21 +0000

    pytest added to PR builds

--- tests/api/services/test_issues_service.py
+++ tests/api/services/test_issues_service.py
@@ -12,10 +12,10 @@ class AMLStreamMock:
         return self
 
     async def __anext__(self):
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.1) # simulate the time between chunks arriving
         if not self._items:
             raise StopAsyncIteration
-        return json.dumps(self._items.pop(0)) # return as string
+        return json.dumps(self._items.pop(0)) # return as json string
 
 @pytest.mark.asyncio
 async def test_get_issues_data(mock_issues_repo, mock_aml_client):
@@ -59,6 +59,18 @@ async def test_initiate_review_valid_chunks(mock_issues_repo, mock_aml_client, d
                         "text": "issue1",
                         "explanation": "explanation1",
                         "suggested_fix": "fix1",
+                     },
+                     {
+                        "type": "Definitive Language",
+                        "location": {
+                            "source_sentence": "sentence2",
+                            "page_num": 2,
+                            "bounding_box": [5.0, 6.0, 7.0, 8.0],
+                            "para_index": 2
+                        },
+                        "text": "issue2",
+                        "explanation": "explanation2",
+                        "suggested_fix": "fix2",
                      }
                 ]
             }
@@ -67,11 +79,49 @@ async def test_initiate_review_valid_chunks(mock_issues_repo, mock_aml_client, d
 
     issues_service = IssuesService(mock_issues_repo, mock_aml_client)
     async for issues in issues_service.initiate_review(doc_name, dummy_user, "2021-09-01"):
+            assert len(issues) == 2
+            assert issues[0].type == IssueType.GrammarSpelling
+            assert issues[1].type == IssueType.DefinitiveLanguage
+
             for issue in issues:
-                assert issue.type == IssueType.GrammarSpelling
                 assert issue.doc_id == doc_name
                 assert issue.status == "not_reviewed"
                 assert issue.review_initiated_by == dummy_user.oid
                 assert issue.review_initiated_at_UTC == "2021-09-01"
 
     mock_aml_client.call_aml_endpoint.assert_called_once_with("", "abc.pdf")
+
+@pytest.mark.asyncio
+async def test_initiate_review_throws_exception_for_bad_chunk(mock_issues_repo, mock_aml_client, dummy_user):
+    """ Checks the initiate review method """
+
+    doc_name = "abc.pdf"
+
+    mock_aml_client.call_aml_endpoint.return_value = AMLStreamMock(
+        [
+            {"issues": 
+                [
+                    {
+                        "type": "Grammar & Spelling",
+                        "location": {
+                            "source_sentence": "sentence1",
+                            "page_num": 1, # no bounding box
+                            "para_index": 1
+                        },
+                        "text": "issue1",
+                        "explanation": "explanation1",
+                        "suggested_fix": "fix1",
+                     }
+                ]
+            }
+        ]
+    )
+
+    issues_service = IssuesService(mock_issues_repo, mock_aml_client)
+
+    with pytest.raises(Exception):
+        async for issue in issues_service.initiate_review(doc_name, dummy_user, "2021-09-01"):
+            pass
+
+    mock_aml_client.call_aml_endpoint.assert_called_once_with("", "abc.pdf")
+    mock_issues_repo.add_issue.assert_not_called() # no database called in error scenario
